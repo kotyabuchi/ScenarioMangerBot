b6369cfe9fd588ed3375a9e299c155e4ba348d16
kotyabuchi <kotyabuchi@gmail.com>
2024-06-30 17:42:22 +0900

    Debug: Koyebでトークンが上手く取得できるかの確認

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const { Client, GatewayIntentBits, Partials, ApplicationCommandType, ApplicationCommandOptionType } = require('discord.js');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages], partials: [Partials.Channel] });
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
+
+console.log(`Login with : ${DISCORD_TOKEN}`);
 
 client.on('ready', () => console.log(`Logged in as ${client.user.tag}!`));
 
